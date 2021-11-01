566bb3d105ca6bb878803997b66b844a867ad461
Patrice CAVEZZAN <pcavezzan@gmail.com>
2021-11-01 09:25:46 +0100

    Upgrade spring boot 2.5.6 - JAVA 17

--- backend/src/main/java/com/github/pcavezzan/kubernetes/workshop/Message.java
+++ backend/src/main/java/com/github/pcavezzan/kubernetes/workshop/Message.java
@@ -1,13 +1,3 @@
 package com.github.pcavezzan.kubernetes.workshop;
 
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@Data
-class Message {
-	private final String payload;
-
-}
+record Message(String payload){}
